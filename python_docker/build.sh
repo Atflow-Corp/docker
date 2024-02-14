58870bd71ca0d34055862dbd13ee5c2da2716d9d
sebatyler <sebatyler@gmail.com>
2024-02-14 22:36:35 +0900

    fix: switch to lowercase

--- python_docker/build.sh
+++ python_docker/build.sh
@@ -2,15 +2,15 @@
 
 name_tag="python_docker:3.11"
 
-docker buildx build --platform linux/amd64 -t Atflow-Corp/$name_tag .
+docker buildx build --platform linux/amd64 -t atflow-corp/$name_tag .
 
 # CR_USER: github user id
 # CR_PAT: https://docs.github.com/en/free-pro-team@latest/packages/managing-container-images-with-github-container-registry/pushing-and-pulling-docker-images#authenticating-to-github-container-registry
 if [[ $CR_USER ]] && [[ $CR_PAT ]]
 then
-  docker tag Atflow-Corp/$name_tag ghcr.io/Atflow-Corp/docker/$name_tag
+  docker tag atflow-corp/$name_tag ghcr.io/atflow-corp/docker/$name_tag
   echo $CR_PAT | docker login ghcr.io -u $CR_USER --password-stdin
-  docker push ghcr.io/Atflow-Corp/docker/$name_tag
+  docker push ghcr.io/atflow-corp/docker/$name_tag
 else
   echo "CR_USER, CR_PAT environment variables required!"
 fi
